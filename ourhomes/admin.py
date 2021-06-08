eefaf6d0480100a5178e07741c9efc9e9618818a
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-08 22:46:21 +0300

    create location serializer

--- ourhomes/admin.py
+++ ourhomes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Home)
+admin.site.register(Location)
+admin.site.register(Profile)
+admin.site.register(Admin)
+admin.site.register(HomeManager)
+admin.site.register(HousePost)
