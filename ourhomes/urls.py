3836d7203dbf7d595b8ac6cf20f636a990f29260
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-08 23:00:43 +0300

    create the api endpoint to call all locations

--- ourhomes/urls.py
+++ ourhomes/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
+# define paths
 urlpatterns=[
-    
+    path('api/locations/', views.LocationList.as_view(), name="locations"),
+
 ]
--- ourhomes/views.py
+++ ourhomes/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import *
-
+from .models import *
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
 # Create your views here.
+class LocationList(APIView):
+    serializer_class=LocationSerializer
+
+    #get all the locations
+    def get(self, request, format=None):
+        locations=Location.objects.all()
+        serializers=LocationSerializer(locations, many=True)
+        return Response(serializers.data)
