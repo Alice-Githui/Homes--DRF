816b82194b1ce301db84276f921e9a1fd3b80dfe
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-08 21:24:16 +0300

    install django cors headers for the dajnago rest framework

--- homes/settings.py
+++ homes/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
 
     # install rest_framework
     'rest_framework',
+    # install corsheaders
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
