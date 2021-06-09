84bc2a922938b13de6e95e7e482887b9398a19e0
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-09 09:35:00 +0300

    update home model with image field

--- ourhomes/models.py
+++ ourhomes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Location(models.Model):
@@ -40,6 +41,7 @@ class HomeManager(models.Model):
         return str(self.user)
 
 class Home(models.Model):
+    image=CloudinaryField('image')
     name=models.CharField(max_length=300)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
     capacity=models.IntegerField(default=0)
