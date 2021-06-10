1e578b27abb76867ebe9c3ccbe8bfecda6e5ecc2
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-10 11:48:38 +0300

    re-run migrations on the database

--- homes/settings.py
+++ homes/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     # install corsheaders
     'corsheaders',
     # cloudinary
-    'cloudinary'
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
--- ourhomes/models.py
+++ ourhomes/models.py
@@ -19,15 +19,20 @@ class User(AbstractUser):
     last_name=models.CharField(max_length=200)
     email=models.EmailField(default="", unique=True)
 
+class Admin(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    location=models.CharField(max_length=300)
+
 def upload_image(instance, filename):
     return "/".join(['images', str(instance.name), filename])
 
 class Home(models.Model):
     # images=models.ImageField(blank=False, null=True, upload_to=upload_image)
-    images=CloudinaryField('image')
+    images=CloudinaryField('image', default="")
     name=models.CharField(max_length=300)
-    location=models.ForeignKey(Location, on_delete=models.CASCADE)
+    location=models.ForeignKey(Location, on_delete=models.CASCADE, default="")
     capacity=models.IntegerField(default=0)
+
     def __str__(self):
         return self.name
 
@@ -35,20 +40,28 @@ class Home(models.Model):
         home=Home.objects.filter(name__icontains=search_term)
         return home
 
+class Manager(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    home=models.ForeignKey(Home, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
+
+
 class HousePost(models.Model):
     name=models.ForeignKey(User, on_delete=models.CASCADE)
     home=models.ForeignKey(Home, on_delete=models.CASCADE, related_name="nameofhome")
     details=models.TextField()
 
     def __str__(self):
-        return self.details
+        return str(self.name_username)
 
 class Post(models.Model):
     name=models.ForeignKey(User, on_delete=models.CASCADE)
     details=models.TextField()
 
     def __str__(self):
-        return self.details
+        return str(self.name_username)
 
 
 
